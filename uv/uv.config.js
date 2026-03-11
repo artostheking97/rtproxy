24ed16549233f720c49ac725b56aa1e3295d0f02
artostheking97 <artostheking97@gmail.com>
2026-03-11 17:22:15 -0600

    Update UV config paths and prefix

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 /*global Ultraviolet*/
 self.__uv$config = {
-    prefix: "/active/uv/service/",
+    prefix: "/service/",
     bare: [
         "https://bare.titaniumnetwork.org/bare/",
         "https://uv.holyubofficial.net/bare/",
@@ -8,9 +8,9 @@ self.__uv$config = {
     ],
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: "/active/uv/uv.handler.js",
-    client: "/active/uv/uv.client.js",
-    bundle: "/active/uv/uv.bundle.js",
-    config: "/active/uv/uv.config.js",
-    sw: "/active/uv/uv.sw.js",
+    handler: "/uv/uv.handler.js",
+    client: "/uv/uv.client.js",
+    bundle: "/uv/uv.bundle.js",
+    config: "/uv/uv.config.js",
+    sw: "/uv/uv.sw.js",
 };
