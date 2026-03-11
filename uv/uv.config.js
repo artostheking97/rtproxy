2998b7f84112d4d2f72c1fca8413145202cf6053
artostheking97 <artostheking97@gmail.com>
2026-03-11 17:02:07 -0600

    Update bare server URL in uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,20 +1,12 @@
 /*global Ultraviolet*/
 self.__uv$config = {
     prefix: '/service/',
-
-    // Array of Bare servers for fallback
-    bare: [
-        'https://bare.titaniumnetwork.org/bare/',
-        'https://bare.px10.workers.dev/bare/',
-        'https://bare.chromehack.com/bare/'
-    ],
-
+    bare: 'https://uv.holyubofficial.net/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-
     handler: '/uv/uv.handler.js',
     client: '/uv/uv.client.js',
     bundle: '/uv/uv.bundle.js',
     config: '/uv/uv.config.js',
-    sw: '/uv/uv.sw.js'
+    sw: '/uv/uv.sw.js',
 };
