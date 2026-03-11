0c0ed412365f6df29399c5c712760b5b25cc8048
artostheking97 <artostheking97@gmail.com>
2026-03-11 17:19:56 -0600

    aifshgbjkasifegbikwqrnutigweahuyiokfgtvukewa

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,12 +1,16 @@
 /*global Ultraviolet*/
 self.__uv$config = {
-    prefix: '/service/',
-    bare: 'https://bare.px10.workers.dev/bare/',
+    prefix: "/active/uv/service/",
+    bare: [
+        "https://bare.titaniumnetwork.org/bare/",
+        "https://uv.holyubofficial.net/bare/",
+        "https://bare.px10.workers.dev/bare/"
+    ],
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/uv/uv.handler.js',
-    client: '/uv/uv.client.js',
-    bundle: '/uv/uv.bundle.js',
-    config: '/uv/uv.config.js',
-    sw: '/uv/uv.sw.js',
+    handler: "/active/uv/uv.handler.js",
+    client: "/active/uv/uv.client.js",
+    bundle: "/active/uv/uv.bundle.js",
+    config: "/active/uv/uv.config.js",
+    sw: "/active/uv/uv.sw.js",
 };
