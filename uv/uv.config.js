8e3efc8a3d5a802f651a6361200182fd78a8d43c
artostheking97 <artostheking97@gmail.com>
2026-03-11 16:20:18 -0600

    Add Bare servers and update fetch handling
    Added a list of Bare servers to the configuration and modified the fetch function to try these servers in order.

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,7 +1,12 @@
 /*global Ultraviolet*/
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://bare.titaniumnetwork.org/bare/',
+    // List of Bare servers to try
+    bareServers: [
+        'https://bare.titaniumnetwork.org/bare/',
+        'https://bare.px10.workers.dev/bare/',
+        'https://bare.chromehack.com/bare/'
+    ],
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
@@ -10,3 +15,24 @@ self.__uv$config = {
     config: '/uv/uv.config.js',
     sw: '/uv/uv.sw.js',
 };
+
+// Override the bare property dynamically to try servers in order
+(function() {
+    let index = 0;
+    const originalFetch = fetch;
+    fetch = function(resource, init) {
+        if (resource.startsWith(self.__uv$config.prefix)) {
+            const tryServer = () => {
+                if (index >= self.__uv$config.bareServers.length) return Promise.reject('All Bare servers failed');
+                const url = resource.replace(self.__uv$config.prefix, self.__uv$config.bareServers[index]);
+                return originalFetch(url, init).catch(() => {
+                    index++;
+                    return tryServer();
+                });
+            };
+            return tryServer();
+        } else {
+            return originalFetch(resource, init);
+        }
+    };
+})();
