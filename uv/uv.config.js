ba3a787db7fae6230df4973bc166cdfbddb05d8e
artostheking97 <artostheking97@gmail.com>
2026-03-11 16:44:50 -0600

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,12 +1,7 @@
 /*global Ultraviolet*/
 self.__uv$config = {
     prefix: '/service/',
-    // List of Bare servers to try
-    bareServers: [
-        'https://bare.titaniumnetwork.org/bare/',
-        'https://bare.px10.workers.dev/bare/',
-        'https://bare.chromehack.com/bare/'
-    ],
+    bare: 'https://bare.titaniumnetwork.org/bare/', // public Bare server
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
@@ -15,24 +10,3 @@ self.__uv$config = {
     config: '/uv/uv.config.js',
     sw: '/uv/uv.sw.js',
 };
-
-// Override the bare property dynamically to try servers in order
-(function() {
-    let index = 0;
-    const originalFetch = fetch;
-    fetch = function(resource, init) {
-        if (resource.startsWith(self.__uv$config.prefix)) {
-            const tryServer = () => {
-                if (index >= self.__uv$config.bareServers.length) return Promise.reject('All Bare servers failed');
-                const url = resource.replace(self.__uv$config.prefix, self.__uv$config.bareServers[index]);
-                return originalFetch(url, init).catch(() => {
-                    index++;
-                    return tryServer();
-                });
-            };
-            return tryServer();
-        } else {
-            return originalFetch(resource, init);
-        }
-    };
-})();
