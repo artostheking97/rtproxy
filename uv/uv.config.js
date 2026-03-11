9a261558e942f62d27cef2e6e1cdb341e2eb4d1f
artostheking97 <artostheking97@gmail.com>
2026-03-11 15:56:11 -0600

    fix

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,7 @@
 /*global Ultraviolet*/
 self.__uv$config = {
     prefix: '/service/',
+    bare: 'https://bare.chromehack.com/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv.handler.js',
