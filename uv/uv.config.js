8611bc4a4b13b1965ec97329f171ff34197a93d0
artostheking97 <artostheking97@gmail.com>
2026-03-11 17:00:32 -0600

    Update Bare server configuration for fallback

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,12 +1,20 @@
 /*global Ultraviolet*/
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://bare.titaniumnetwork.org/bare/', // public Bare server
+
+    // Array of Bare servers for fallback
+    bare: [
+        'https://bare.titaniumnetwork.org/bare/',
+        'https://bare.px10.workers.dev/bare/',
+        'https://bare.chromehack.com/bare/'
+    ],
+
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
+
     handler: '/uv/uv.handler.js',
     client: '/uv/uv.client.js',
     bundle: '/uv/uv.bundle.js',
     config: '/uv/uv.config.js',
-    sw: '/uv/uv.sw.js',
+    sw: '/uv/uv.sw.js'
 };
