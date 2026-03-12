374981b6b89c05289d15d3d4c7fd9c7dfeacaece
artostheking97 <artostheking97@gmail.com>
2026-03-11 20:24:48 -0600

    Update UV config prefix and formatting

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,16 +1,16 @@
 /*global Ultraviolet*/
 self.__uv$config = {
-    prefix: "/uv/service/",
-    bare: [
-        "https://bare.titaniumnetwork.org/bare/",
-        "https://uv.holyubofficial.net/bare/",
-        "https://bare.px10.workers.dev/bare/"
-    ],
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: "/uv/uv.handler.js",
-    client: "/uv/uv.client.js",
-    bundle: "/uv/uv.bundle.js",
-    config: "/uv/uv.config.js",
-    sw: "/uv/uv.sw.js",
+  prefix: "/service/",
+  bare: [
+    "https://bare.titaniumnetwork.org/bare/",
+    "https://uv.holyubofficial.net/bare/",
+    "https://bare.px10.workers.dev/bare/"
+  ],
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: "/uv/uv.handler.js",
+  client: "/uv/uv.client.js",
+  bundle: "/uv/uv.bundle.js",
+  config: "/uv/uv.config.js",
+  sw: "/uv/uv.sw.js",
 };
